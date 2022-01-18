c6b7b42ef7b6a67ab930ce659951b9c249c45fad
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-01-18 13:34:38 +0100

    feat(post-donnee): contenu sans implementation

--- app/src/main/java/com/example/capteurenvoiedonnees/ui/sample/SampleFragment.kt
+++ app/src/main/java/com/example/capteurenvoiedonnees/ui/sample/SampleFragment.kt
@@ -1,11 +1,14 @@
 package com.example.capteurenvoiedonnees.ui.sample
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.capteurenvoiedonnees.R
 import com.example.capteurenvoiedonnees.databinding.FragmentSampleBinding
 
 class SampleFragment: Fragment() {
@@ -22,5 +25,13 @@ class SampleFragment: Fragment() {
         _binding = FragmentSampleBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button: Button = view.findViewById(R.id.button_send)
+        button.setOnClickListener {
+            Log.d("button", "bouton cliqué !")
+        }
     }
 }
