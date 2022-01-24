46ee9883c5babdd7e7f4e67f1bd8826390e03cab
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-01-24 16:49:15 +0100

    feat(post-donnee_complet): create data class to define data structure

---
+++ app/src/main/java/com/example/capteurenvoiedonnees/model/Sample.kt
@@ -0,0 +1,8 @@
+package com.example.capteurenvoiedonnees.model
+
+data class Sample(
+    val localDateTime: String,
+    val measurementName: String,
+    val unit: String,
+    val value: Number
+)
