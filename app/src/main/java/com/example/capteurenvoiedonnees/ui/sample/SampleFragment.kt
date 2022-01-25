abb463f33631580eb47cf62362af4dce65cf6c29
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-01-25 22:19:13 +0100

    feat(detection-position-assise): base pour projet de detection d'une position assise

--- app/src/main/java/com/example/capteurenvoiedonnees/ui/sample/SampleFragment.kt
+++ app/src/main/java/com/example/capteurenvoiedonnees/ui/sample/SampleFragment.kt
@@ -28,9 +28,16 @@ class SampleFragment: Fragment(), SensorEventListener {
         Sensor.TYPE_GYROSCOPE
     )
 
-    private var currentX: TextView? = null
-    private var currentY:TextView? = null
-    private var currentZ:TextView? = null
+    private var accX: TextView? = null
+    private var accY:TextView? = null
+    private var accZ:TextView? = null
+
+    private var gyroX: TextView? = null
+    private var gyroY:TextView? = null
+    private var gyroZ:TextView? = null
+
+    private var numberOfSit:TextView? = null
+    private var currentMove:TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -62,9 +69,19 @@ class SampleFragment: Fragment(), SensorEventListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        currentX = _binding?.root?.findViewById<View>(R.id.currentX) as TextView
-        currentY = _binding?.root?.findViewById<View>(R.id.currentY) as TextView
-        currentZ = _binding?.root?.findViewById<View>(R.id.currentZ) as TextView
+        accX = _binding?.root?.findViewById<View>(R.id.accX) as TextView
+        accY = _binding?.root?.findViewById<View>(R.id.accY) as TextView
+        accZ = _binding?.root?.findViewById<View>(R.id.accZ) as TextView
+
+        gyroX = _binding?.root?.findViewById<View>(R.id.gyroX) as TextView
+        gyroY = _binding?.root?.findViewById<View>(R.id.gyroY) as TextView
+        gyroZ = _binding?.root?.findViewById<View>(R.id.gyroZ) as TextView
+
+        numberOfSit = _binding?.root?.findViewById<View>(R.id.numberOfSit) as TextView
+        currentMove = _binding?.root?.findViewById<View>(R.id.current_move) as TextView
+
+        numberOfSit?.text = "0"
+        currentMove?.text = getString(R.string.unknown_move)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -73,10 +90,18 @@ class SampleFragment: Fragment(), SensorEventListener {
             val yValue = event.values[1]
             val zValue = event.values[2]
             Log.d("SENSOR", "${event.sensor.stringType} : x=$xValue, y=$yValue, z=$zValue")
+            // traitement seulement si les valeurs proviennent de l'acceleromètre
             if (event.sensor.stringType.equals("android.sensor.linear_acceleration")) {
-                currentX?.text = "$xValue"
-                currentY?.text = "$yValue"
-                currentZ?.text = "$zValue"
+                // assigne les valeurs du capteur aux textView respective
+                accX?.text = "$xValue"
+                accY?.text = "$yValue"
+                accZ?.text = "$zValue"
+            }
+            if (event.sensor.stringType.equals("android.sensor.gyroscope")) {
+                // assigne les valeurs du capteur aux textView respective
+                gyroX?.text = "$xValue"
+                gyroY?.text = "$yValue"
+                gyroZ?.text = "$zValue"
             }
         }
     }
