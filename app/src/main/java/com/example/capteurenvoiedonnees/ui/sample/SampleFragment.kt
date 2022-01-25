9c894ae7005ec6341c8b88dbc940d44b6f71bd48
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2022-01-25 15:58:44 +0100

    feat(detection-position-assise): base pour projet de detection d'une position assise

--- app/src/main/java/com/example/capteurenvoiedonnees/ui/sample/SampleFragment.kt
+++ app/src/main/java/com/example/capteurenvoiedonnees/ui/sample/SampleFragment.kt
@@ -22,11 +22,12 @@ class SampleFragment: Fragment(), SensorEventListener {
 
     private var sensorManager: SensorManager? = null
     private var accelerometer: Sensor? = null
+    private var linearAcceleration: Sensor? = null
 
     private var currentX: TextView? = null
     private var currentY:TextView? = null
     private var currentZ:TextView? = null
-    
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,19 +38,15 @@ class SampleFragment: Fragment(), SensorEventListener {
         sensorManager = this.activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
         sensorManager?.let { sensorManager ->
-            if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
-                accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
-                sensorManager.registerListener(
-                    this,
-                    accelerometer,
-                    SensorManager.SENSOR_DELAY_NORMAL
-                )
+            if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null && sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+                accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+                linearAcceleration = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+                sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+                sensorManager.registerListener(this, linearAcceleration, SensorManager.SENSOR_DELAY_NORMAL)
             } else {
-                Toast.makeText(context, "ACCELEROMETER SENSOR NOT AVAILABLE", Toast.LENGTH_LONG)
-                    .show()
+                Toast.makeText(context, "LINEAR ACCELERATION OR ACCELEROMETER SENSORS NOT AVAILABLE", Toast.LENGTH_LONG).show()
             }
         }
-        print("ACCELEROMETER" + "create")
         return binding.root
     }
 
@@ -58,7 +55,6 @@ class SampleFragment: Fragment(), SensorEventListener {
         currentX = _binding?.root?.findViewById<View>(R.id.currentX) as TextView
         currentY = _binding?.root?.findViewById<View>(R.id.currentY) as TextView
         currentZ = _binding?.root?.findViewById<View>(R.id.currentZ) as TextView
-        print("ACCELEROMETER" + "test")
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -66,12 +62,14 @@ class SampleFragment: Fragment(), SensorEventListener {
             val xValue = event.values[0]
             val yValue = event.values[1]
             val zValue = event.values[2]
-            Log.d("ACCELEROMETER", "$xValue ; $yValue ; $zValue")
+            Log.d("SENSOR", "${event.sensor.stringType} : x=$xValue, y=$yValue, z=$zValue")
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        Log.d("ACCELEROMETER", "$accuracy")
+        if (sensor != null) {
+            Log.d("SENSOR ACC", "${sensor.stringType} : accuracy = $accuracy")
+        }
     }
 
     override fun onResume() {
